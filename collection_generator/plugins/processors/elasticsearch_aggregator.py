7b791eb5eeabec7ca7256ca8bcd28302eab4d5c3
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-11-12 16:48:02 +0000

    list extra top level facets

--- collection_generator/plugins/processors/elasticsearch_aggregator.py
+++ collection_generator/plugins/processors/elasticsearch_aggregator.py
@@ -240,8 +240,8 @@ class ElasticsearchAggregator(BaseAggregationProcessor):
 
         metadata = {}
 
-        # Get list of aggregation facets
-        facets = description.aggregation_facets
+        # Get list of aggregation facets and extra top level facets
+        facets = description.aggregation_facets + description.extra_facets
 
         # Poll elasticsearch for value list for each facet
         summaries = {}
